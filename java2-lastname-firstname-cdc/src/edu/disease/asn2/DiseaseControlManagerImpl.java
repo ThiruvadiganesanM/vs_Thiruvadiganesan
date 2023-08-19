c7cd664bb8bd5ef75e5b29b512ff25464528b3ee
ThiruvadiganesanM <138756571+ThiruvadiganesanM@users.noreply.github.com>
2023-08-19 22:01:36 +0530

    Update DiseaseControlManagerImpl.java

--- java2-lastname-firstname-cdc/src/edu/disease/asn2/DiseaseControlManagerImpl.java
+++ java2-lastname-firstname-cdc/src/edu/disease/asn2/DiseaseControlManagerImpl.java
@@ -6,7 +6,7 @@ import edu.disease.asn1.Exposure;
 
 public class DiseaseControlManagerImpl implements DiseaseControlManager{
 	int maxDiseases,maxPatients;
-	int diseaseCount=0;
+	int diseaseCount=0, patientCount=0;
 	public DiseaseControlManagerImpl(int maxDiseases,int maxPatients) {
 		if(maxDiseases <=0 && maxPatients <=0) {
 			try {
@@ -31,10 +31,10 @@ public class DiseaseControlManagerImpl implements DiseaseControlManager{
 			}
 		}
 		if(isInfectious) {
-			disease=new InfectiousDisease(null,diseaseName);
+			disease=new InfectiousDisease(UUID.randomUUID(),diseaseName);
 		}
 		else {
-			disease=new NonInfectiousDisease(null, diseaseName);
+			disease=new NonInfectiousDisease(UUID.randomUUID(), diseaseName);
 		}
 		if(disease!=null) {
 			diseases[diseaseCount]=disease;
@@ -52,7 +52,7 @@ public class DiseaseControlManagerImpl implements DiseaseControlManager{
 		}
 		if(disease1==null) {
 			try {
-			throw new NullPointerException();
+			throw new NullPointerException("there is no such disease is in that id");
 			}catch(NullPointerException e) {
 				e.printStackTrace();
 			}
@@ -61,21 +61,90 @@ public class DiseaseControlManagerImpl implements DiseaseControlManager{
 	}
 
 	public Patient addPatient(String firstName, String lastName, int maxDiseases, int maxExposures) {
-		return null;
+		Patient p1=new Patient(maxDiseases,maxExposures);
+		p1.setPatientId(UUID.randomUUID());
+		if(patientCount==patient.length) {
+			try {
+			throw new IllegalStateException("Patient list is full");
+			}
+			catch(IllegalStateException e) {
+				e.printStackTrace();
+			}
+		}else {
+			patient[patientCount++]=p1;
+		}
+		return p1;
 	}
 
 	public Patient getPatient(UUID patientId) {
-		// TODO Auto-generated method stub
-		return null;
+		Patient p1=null;
+		for(int i=0;i<patient.length;i++) {
+			if(patientId==patient[i].patientId) {
+				p1=patient[i];
+			}
+		}
+		if(p1==null) {
+			try {
+			throw new NullPointerException("No such patient is there in that id");
+			}
+			catch(NullPointerException e) {
+				e.printStackTrace();
+			}
+		}
+		return p1;
+		
 	}
 
 	public void addDiseaseToPatient(UUID patientId, UUID diseaseId) {
-		// TODO Auto-generated method stub
+		Patient pat=null;
+	for(int i=0;i<patient.length;i++) {
+		if(patientId==patient[i].patientId) {
+			pat=patient[i];
+		}
+	}
+	if(pat==null) {
+		try {
+		throw new NullPointerException("No such patient is there in that id");
+		}
+		catch(NullPointerException e) {
+			e.printStackTrace();
+		}
+	}
+	Disease dis=null;
+	for(int i=0;i<diseases.length;i++) {
+		if(diseaseId==diseases[i].diseasesId) {
+			dis=diseases[i];
+		}
+	}
+	if(dis==null) {
+		try {
+		throw new NullPointerException("No such disease is there in that id");
+		}catch(NullPointerException e) {
+			e.printStackTrace();
+		}
+	}
+	pat.addDiseaseId(diseaseId);
+	
 		
 	}
 
 	public void addExposureToPatient(UUID patientId, Exposure exposure) {
-		// TODO Auto-generated method stub
+		Patient patie=null;
+		for(int i=0;i<patient.length;i++) {
+			if(patientId==patient[i].patientId) {
+				patie=patient[i];
+			}
+		}
+		if(patie==null) {
+			try {
+			throw new NullPointerException("No such patient is there in that id");
+			}
+			catch(NullPointerException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		patie.addExposure(exposure);
 		
 	}
 }
